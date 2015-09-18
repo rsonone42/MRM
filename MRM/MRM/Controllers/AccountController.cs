b605cc4b4fd5a98930e0c996b6e7126a607f73c0
rsonone42 <rsonone42@gmail.com>
2015-09-18 17:28:08 +0530

    dasd

--- MRM/MRM/Controllers/AccountController.cs
+++ MRM/MRM/Controllers/AccountController.cs
@@ -13,7 +13,7 @@ using MRM.Models;
 namespace MRM.Controllers
 {
     [Authorize]
-    public class AccountController : Controller
+    public class AccountController : Controller 
     {
         private ApplicationSignInManager _signInManager;
         private ApplicationUserManager _userManager;
